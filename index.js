a2d3479431514aa94028d40226de0e0b917540d7
Paulo Benditoe <paulobenedito@ufl.edu>
2022-06-14 16:20:04 -0700

    Assignment 3

---
+++ index.js
@@ -0,0 +1,49 @@
+// Your code here
+
+
+        var questionsArr = [
+            {
+                question: 'The capital of Bolivia is La Paz',
+                answer: true
+              },
+
+            {
+                question: 'The capital of Brazil is Sao Paulo',
+                answer: false
+              },
+              
+            {
+                question: 'The capital of Japan is Tokyo',
+                answer: true
+              },
+              
+            {
+                question: 'capital of Portugal is Lisbon',
+                answer: true
+              },
+              
+            {
+                question: 'The capital of United Stated is New York',
+                answer: false
+              },   
+        ]
+
+
+        function runQuiz () {
+            var score = 0
+        
+            for (var i = 0; i < questionsArr.length; i++){
+              var playerAnswer = confirm(questionsArr[i].question)
+
+              if(playerAnswer === questionsArr[i].answer){
+                score++
+              }
+            }
+
+              var finalScore = Math.round(score / questionsArr.length * 100)
+
+              alert('Your Scroe Was' + finalScore + '%')
+             
+        }
+
+
